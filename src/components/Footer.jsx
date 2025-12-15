c192f8a717ea2c42fa00d8fc8a43b2e45fb278bf
Yusufi Abdurrahman <66905124+ab-yusufi@users.noreply.github.com>
2025-12-15 13:04:55 +0530

    Change footer text
    Updated footer text to include author name.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ export const Footer = () => {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       {" "}
-      <p className="text-sm text-muted-foreground">
+      <p className="text-sm text-muted-foreground text-center>
         {" "}
-        &copy; {new Date().getFullYear()} Pedrotech.co. All rights reserved.
+        Abdurrahman Yusufi aka AB Coder
       </p>
       <a
         href="#hero"
