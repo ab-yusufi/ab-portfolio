0d461aea6f6345523641c46230cdd10aa18a5898
Yusufi Abdurrahman <66905124+ab-yusufi@users.noreply.github.com>
2025-12-15 13:10:49 +0530

    Fix formatting in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       {" "}
-      <p className="text-sm text-muted-foreground text-center>
+      <p className="text-sm text-muted-foreground text-center">
         {" "}
         Abdurrahman Yusufi aka AB Coder
       </p>
